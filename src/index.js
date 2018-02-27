e24ce3f4492cac6607cf4999629c3c117d0f003e
OsmolovskiiSergei <osmolovskii_sergei@mail.ru>
2018-02-27 19:52:31 +0300

    zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+let a = 0;
+    while (number>0) {
+    number = Math.floor(number / 5);
+    a += number;
+  } 
+  return a;}
